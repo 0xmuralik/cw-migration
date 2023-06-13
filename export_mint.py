aa2d1fe8f98b9fb1d62ffc9b632c60e610d0b20c
0xmuralik <krishna192reddy@gmail.com>
2023-06-13 17:00:31 +0530

    minter data export

--- export_mint.py
+++ export_mint.py
@@ -12,8 +12,13 @@ def append_to_json_array(json_array, json_object):
     json_array.append(json_object)
 
 def decode_obj(json_object):
-    json_object["key"]=bytes.fromhex(json_object.get("key")).decode()
+    try:
+        json_object["key"]=bytes.fromhex(json_object.get("key")).decode()
+    except UnicodeDecodeError:
+        1+1
+
     json_object["value"]=base64.b64decode((json_object).get("value")).decode()
+
     return json_object
 
 def convert_addr(addr):
@@ -22,10 +27,12 @@ def convert_addr(addr):
 
 def main():
     # Existing JSON array
-    existing_array = []
+    # tokens = []
+    rest = []
+    minters =[]
 
     # URL with pagination
-    url = "https://juno.stakesystems.io/cosmwasm/wasm/v1/contract/juno1za0uemnhzwkjrqwguy34w45mqdlzfm9hl4s5gp5jtc0e4xvkrwjs6s2rt4/state"
+    url = "https://juno.stakesystems.io/cosmwasm/wasm/v1/contract/juno15yalm0qgg0wzs4etkzzputy7hum8ryzc0rrfzfplnqjeyfrpufwq9u2zwd/state"
     total = fetch_json(url).get("pagination").get("total")
     # Pagination loop
     pagination_key = ""
@@ -44,38 +51,23 @@ def main():
         for json_object in json_objects:
             json_object=decode_obj(json_object)
 
-            # Replace Juno address with pasg address in keys
-            key=json_object.get("key")
-            i=key.find("juno")
-            if i != -1 :
-                juno_addr=key[i:-5]
-                pasg_addr=convert_addr(juno_addr)
-                key=key.replace(juno_addr,pasg_addr)
-                json_object["key"]=key
-            
-            elif "\u0000\u0006" in key:
-                val=json.loads(json_object.get("value"))
-                val["owner"]=convert_addr(val["owner"])
-                for approval in val["approvals"]:
-                    approval["spender"]=convert_addr(approval["spender"])
-                    
-                json_object["value"]=json.dumps(val)
-            
-            elif "minter" == key:
-                val=json.loads(json_object.get("value"))
-                json_object["value"]=json.dumps(convert_addr(val))
+            if json_object.get("key").startswith("\u0000\u000eminter_address"):
+                address=convert_addr(json_object.get("key")[16:])
+                minter={"address":address,"mints":json_object.get("value")}
+                append_to_json_array(minters,minter)
+            else:
+                append_to_json_array(rest, json_object)
 
-            append_to_json_array(existing_array, json_object)
 
         # Check if there are more pages
         pagination_key = response.get("pagination").get("next_key")
         if not pagination_key:
             break
     
-    models = {"models": existing_array}
+    models = {"minters":minters,"models":rest}
 
     # Save the updated JSON array to a file
-    with open("output.json", "w") as file:
+    with open("mint.json", "w") as file:
         json.dump(models, file,indent=4)
 
 if __name__ == "__main__":
