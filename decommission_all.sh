b2b9e5d8e79d70c168b734f9e13205e4df3f9c67
0xmuralik <krishna192reddy@gmail.com>
2023-07-25 13:15:29 +0530

    decommission_all.sh

---
+++ decommission_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CONTRACTS=$(jq ".contracts" ./contracts.json)
+len=$(jq ".contracts | length" ./contracts.json)
+
+cd ./juno || exit
+for ((i=0;i<len;i++)); do
+    name=$(echo "$CONTRACTS" | jq -r ' .['$i'] | .name')
+    nft=$(echo "$CONTRACTS" | jq -r ' .['$i'] | .nft')
+    mint=$(echo "$CONTRACTS" | jq -r ' .['$i'] | .mint')
+    market=$(echo "$CONTRACTS" | jq -r ' .['$i'] | .marketplace')
+
+    echo "Migrating $name contract"
+    sed -i "s/^name=.*/name=$name/" .env
+    sed -i "s/^nft_address=.*/nft_address=$nft/" .env
+    sed -i "s/^minter_address=.*/minter_address=$mint/" .env
+    sed -i "s/^marketplace_address=.*/marketplace_address=$market/" .env
+    
+    ./decommission.sh
+done
--- juno/update_nft_contract.sh
+++ juno/update_nft_contract.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
-CHAINID="junod-1"
-KEY="mykey"
-PATH_TO_CONTRACTS="/home/vitwit/passage/passage-contracts"
-CURRENT_DIR=$(pwd)
-
 source .env
 
 cd $PATH_TO_CONTRACTS || exit
-git stash
 git switch murali/decommission
 cd "$CURRENT_DIR" || exit
 
-echo "Deploying new pg721 metadata onchain contract..."
+echo "$name: Deploying new pg721 metadata onchain contract..."
 junod tx wasm store "$PATH_TO_CONTRACTS"/artifacts/pg721_metadata_onchain.wasm --from $KEY --gas auto --gas-adjustment 1.15 --chain-id $CHAINID -y -b block
 
 NFT_CODE_ID=$(junod query wasm list-code --output json | jq -r '.code_infos[-1].code_id')
-sed -i "s/^new_nft_code_id=.*/new_nft_code_id=$NFT_CODE_ID/" .env
 
-junod tx wasm migrate "$new_nft_address" "$NFT_CODE_ID" '{"minter":"'$new_minter_contract'"}' --from $KEY --chain-id $CHAINID --gas auto --gas-adjustment 1.15 -y -b block 
-
-echo "Minting token"
-junod tx wasm execute "$new_minter_contract" '{"mint":{}}' --amount 100stake --from $KEY --chain-id $CHAINID --gas auto --gas-adjustment 1.15 -y -b block
-
-UPDATE='{
-    "update_per_address_limit":{
-        "per_address_limit": 10
-    }
-}'
-
-echo "update per person limit"
-junod tx wasm execute "$new_minter_contract" "$UPDATE" --amount 100stake --from $KEY --chain-id $CHAINID --gas auto --gas-adjustment 1.15 -y -b block
+echo "$name: Migrating pg721 metadata onchain contract for decommission..."
+junod tx wasm migrate "$nft_address" "$NFT_CODE_ID" '{"minter":""}' --from $KEY --chain-id $CHAINID --gas auto --gas-adjustment 1.15 -y -b block 
